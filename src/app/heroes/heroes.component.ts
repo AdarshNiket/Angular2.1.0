f8324b3a39a4fb78a5c78ece1597543805561748
AdarshNiket <adarsh.niket@ge.com>
2018-06-07 21:16:48 +0530

    Adding heroes unordered list and mock hero json

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  ngOnInit() {}
+  hero : Hero = {
+  	id : 1,
+  	name : 'Windstorm'
+  };
 
-  ngOnInit() {
-  }
+  heroes = HEROES;
 
 }
